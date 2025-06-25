580a7762387cc303cbc5f5da5e835134d4861ff2
Fabiana Severin <fabisev@amazon.com>
2025-06-25 11:15:56 +0100

    Adding utils file

---
+++ utils.js
@@ -0,0 +1,17 @@
+/**
+ * Utility function to test pipeline functionality
+ */
+
+/**
+ * Performs a simple addition
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Sum of a and b
+ */
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  add
+};
